4895554804fdfd7027eb15277933d4b3ae9fa1c5
OlegSuperBro <olegrakovic323@gmail.com>
2023-10-02 10:38:50 +0300

    added search

--- SportStore/MainWindow.xaml.cs
+++ SportStore/MainWindow.xaml.cs
@@ -83,5 +83,16 @@ namespace SportStore
                 }
             }
         }
+        private void searchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            using (SportStoreContext db = new SportStoreContext())
+            {
+                if (searchBox.Text.Length > 0)
+                {
+                    productlistView.ItemsSource = db.Products.Where(u => u.Name.Contains(searchBox.Text) || u.Description.Contains(searchBox.Text)).ToList();
+                }
+
+            }
+        }
     }
 }
